000a257afe18c7ac8f9df786027160a5e0de3c09
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2021-09-11 14:41:11 -0300

    Implementing ElseIfBlocks component

---
+++ poc/src/components/ElseIfBlocks/ElseIfBlocks.svelte
@@ -0,0 +1,19 @@
+<script>
+    let x = 11;
+</script>
+
+<div class="container">
+    {#if x > 10}
+        <p>{x} é maior que 10</p>
+    {:else if x < 5}
+        <p>5 é maior que {x}</p>
+    {:else}
+        <p>{x} está entre 5 e 10</p>
+    {/if}
+</div>
+
+<style>
+    .container {
+        margin-top: 20px;
+    }
+</style>
