4b18c4d3c5a67a221a6386c19de106de8c6cb290
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2021-09-13 14:21:42 -0300

    Implemeting ElseBlock component using svelte

---
+++ poc/src/components/ElseBlocks/ElseBlocks.svelte
@@ -0,0 +1,24 @@
+<script>
+    let user = {
+        loggedIn: false
+    }
+
+    function toggle() {
+        user.loggedIn = !user.loggedIn
+    }
+</script>
+
+<div class="container">
+    <strong>Using else block</strong>
+    {#if user.loggedIn}
+        <button type="button" on:click={toggle}>Logout</button>
+    {:else}
+        <button type="button" on:click={toggle}>Login</button>
+    {/if}
+</div>
+
+<style>
+    .container {
+        margin-top: 20px 0 0 0;
+    }
+</style>
