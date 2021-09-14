d1111a2b5623987bc995a15b61cb4e26f7fc27f6
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2021-09-14 14:16:51 -0300

    Implementing IfBlock component applying if statement condition using sveltejs

---
+++ poc/src/components/IfBlocks/IfBlock.svelte
@@ -0,0 +1,24 @@
+<script>
+    let user = {
+        loggedIn: false
+    }
+
+    function toggle() {
+        user.loggedIn = !user.loggedIn;
+    }
+</script>
+
+<div class="container">
+    {#if user.loggedIn}
+        <button type="button" on:click={toggle}>Logout</button>
+    {/if}
+    {#if !user.loggedIn}
+        <button type="button" on:click={toggle}>Login</button>
+    {/if}
+</div>
+
+<style>
+    .container {
+        margin-top: 20px;
+    }
+</style>
