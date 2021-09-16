42bb792088d0cfa50b28983eb93e52359dfa43ce
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2021-09-16 12:56:14 -0300

    new adjustments in ParentComponent using spread props

--- poc/src/components/ParentComponent/ParentComponent.svelte
+++ poc/src/components/ParentComponent/ParentComponent.svelte
@@ -1,9 +1,18 @@
 <script>
     import ChildComponent from "../ChildComponent/ChildComponent.svelte";
+    import OtherChildComponent from "../OtherChildComponent/OtherChildComponent.svelte";
+
+    const data = {
+        name: 'Thadeu Munhóz Cesário',
+        age: 27,
+        occupation: 'Software Developer',
+        country: 'Brazil'
+    }
 </script>
 
 <div class="container">
     <ChildComponent name="Parent props"/>
+    <OtherChildComponent {...data} />
 </div>
 
 <style>
