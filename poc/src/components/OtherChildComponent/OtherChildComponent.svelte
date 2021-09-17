ab763f8f06c6b612f19ff93dcbe707b9cbb13b34
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2021-09-17 12:55:48 -0300

    implemeting OtherChildComponent using spread props

---
+++ poc/src/components/OtherChildComponent/OtherChildComponent.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let name;
+    export let age;
+    export let occupation;
+    export let country;
+</script> 
+
+<div>
+    <pre>
+        My name is {name}.
+        I am {age} years old.
+        My occupation is {occupation}.
+        I'm from {country}.
+    </pre>
+</div>
