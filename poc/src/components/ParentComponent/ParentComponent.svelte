d17a3f02a084343653335574221b29a46fd65561
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2021-11-07 12:43:48 -0300

    implementing parent component props example

---
+++ poc/src/components/ParentComponent/ParentComponent.svelte
@@ -0,0 +1,13 @@
+<script>
+    import ChildComponent from "../ChildComponent/ChildComponent.svelte";
+</script>
+
+<div class="container">
+    <ChildComponent name="Parent props"/>
+</div>
+
+<style>
+    .container {
+        margin-top: 12px;
+    }
+</style>
