e44748d13664740868db3b1e4a5b92cdd81434a6
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2021-11-10 12:33:21 -0300

    implement ReactiveStatements component

---
+++ poc/src/components/ReactiveStatements/ReactiveStatements.svelte
@@ -0,0 +1,17 @@
+<script>
+    let count = 0;
+
+    $: if(count > 9) {
+            alert("Numero maximo atingido")
+            count = 9;
+        }
+
+    function handleClick() {
+        count += 1;
+    }
+</script>
+
+<div>
+    <button type="button" on:click={handleClick}>Incrementar</button>
+    <strong>{count}</strong>
+</div>
